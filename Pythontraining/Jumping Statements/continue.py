c13d819a4537c477a86ad1874c49d5f52b7f27ec
Somanath1992 <somanathjadhav1992@gmail.com>
2024-05-13 18:12:26 +0530

    Updated new learnings

---
+++ Pythontraining/Jumping Statements/continue.py
@@ -0,0 +1,5 @@
+for i in range(1, 10):
+    if i == 3 or i == 5 or i == 7:
+        continue  # It will skip the above values and go for next value of i once condition is matched
+    print(i)
+print("Program Exited")
