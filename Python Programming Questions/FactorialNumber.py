5012381c85a859a3f25a0c0dd628dfd055ce7aa6
Somanath1992 <somanathjadhav1992@gmail.com>
2024-06-29 20:09:38 +0530

    Added two approaches for factorial number

--- Python Programming Questions/FactorialNumber.py
+++ Python Programming Questions/FactorialNumber.py
@@ -1,5 +1,6 @@
 # Program to find of factorial(!) of number
 # Factorial of a non-negative integer is the multiplication of all positive integers smaller than or equal to n
+# Approach 1: Using Looping Statement
 def factorial_num(num):
     factorial = 1
     if num < 0:
@@ -15,3 +16,23 @@ def factorial_num(num):
 number = int(input("Enter number to find factorial:"))
 factorial_num(number)
 
+
+# Approach 2: Using recursive(function calls itself) function
+def fact_num(num1):
+    if num1 == 0 or num1 == 1:
+        return 1
+    else:
+        return num1 * fact_num(num1 - 1)
+
+
+number_1 = int(input("Enter number to find factorial:"))
+print("The factorial of ", number_1, "is", fact_num(number_1))
+
+
+# Approach 3: Using Ternary Operator (if else in single line)
+def fact_number(num2):
+    return 1 if num2 == 0 or num2 == 1 else num2 * fact_number(num2 - 1)
+
+
+number_2 = int(input("Enter number to find factorial:"))
+print("The factorial of", number_2, "is", fact_num(number_2))
